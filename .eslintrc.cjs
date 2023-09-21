e965033787a54a9b193f81988d5be204c5a0e13c
timkinsman <tkinsm@gmail.com>
2023-09-21 18:22:34 +0930

    feat: add notifications store

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,16 +5,11 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
