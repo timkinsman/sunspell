798e967a823e157f3bca6ee745c2fcfa44888065
timkinsman <tkinsm@gmail.com>
2024-02-11 18:46:16 +1030

    integrate msw

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export const API_URL = import.meta.env.VITE_API_URL as string;
 export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID as string;
 export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI as string;
+export const VITE_API_MOCKING = import.meta.env.VITE_API_MOCKING as string;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,20 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { VITE_API_MOCKING } from './config';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+async function enableMocking() {
+  if (VITE_API_MOCKING !== 'true') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+});
